ea1e03a04beb1e24ab960967e5e5e753a31e572b
Patrick Cremin <patrickfxcremin@gmail.com>
2020-05-23 20:50:42 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,4 @@
 def display_board(board)
-  
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -7,5 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_indes(user)
-  
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
